9061a68990a205b9db48ba37b1a5e3c17512099c
Tejasvi Bihari <tejasvibihari2000@gmail.com>
2023-12-03 18:48:37 +0530

    Outputting Dynamic value

--- 01-starting-project/src/App.jsx
+++ 01-starting-project/src/App.jsx
@@ -1,10 +1,15 @@
+const reactDescription = ["Fundamental", "Crucial", "Core"];
+
+function genRandomDescription(max) {
+  return Math.floor(Math.random() * (max + 1));
+}
 function Header() {
   return (
     <header>
       <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
       <h1>React Essentials</h1>
       <p>
-        Fundamental React concepts you will need for almost any app you are
+        {reactDescription[genRandomDescription(2)]} React concepts you will need for almost any app you are
         going to build!
       </p>
     </header>
