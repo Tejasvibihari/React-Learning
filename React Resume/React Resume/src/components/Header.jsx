65837da7981cffa819fa67713e3a1c347b0a3320
Tejasvi Bihari <tejasvibihari2000@gmail.com>
2023-12-27 22:33:01 +0545

    created react resume project for practice

---
+++ React Resume/React Resume/src/components/Header.jsx
@@ -0,0 +1,12 @@
+export default function Header() {
+    return (
+        <div>
+            <nav className="bg-dark border-bottom border-body">
+                <div className="container nav-container">
+                    <div className="navbar-brand">Tejasvi</div>
+                    <a href="#" className="headerBtn">Contact Me</a>
+                </div>
+            </nav>
+        </div>
+    );
+}
